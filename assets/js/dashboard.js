01acbd3a2bfde20c4dd894784e778820919cd1a8
Ivan <ivanwong810520@gmail.com>
2023-12-19 15:43:02 +0800

    Initial project setup
    - Initialized folder structure
    - Created main layout for the project

---
+++ assets/js/dashboard.js
@@ -0,0 +1,5 @@
+class Dashboard {
+    static initDashboard() {
+        
+    }
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes/header.php"); ?>
+
+<body id="ims__body">
+    <!-- Main Content -->
+    <main id="ims__main-content">
+    </main>
+</body>
+
+</html>
