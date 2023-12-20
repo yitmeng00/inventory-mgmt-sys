c0d2c8e7d56979feda1ca37a6f07e42edf27c39c
Ivan <ivanwong810520@gmail.com>
2023-12-20 13:44:19 +0800

    Added product, supplier, and transaction page

---
+++ transaction.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include("includes/header.php");
+?>
+
+<body>
+    <div class="ims__body-container d-flex">
+        <?php include("includes/navbar.php"); ?>
+
+        <main class="ims__main-content overflow-y-scroll w-100 p-3">
+            <div>
+                <div>
+                    <p>
+                        Transaction page.
+                    </p>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
