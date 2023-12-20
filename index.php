77a630c13d059888d6b0e4d528e7da5b541db8dc
Ivan <ivanwong810520@gmail.com>
2023-12-20 13:43:22 +0800

    Implemented navbar toggler

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include("includes/header.php"); ?>
+<?php
+$include_chartjs = true;
+include("includes/header.php");
+?>
 
-<body id="ims__body">
-    <!-- Main Content -->
-    <main id="ims__main-content">
-    </main>
+<body>
+    <div class="ims__body-container d-flex">
+        <?php include("includes/navbar.php"); ?>
+
+        <main class="ims__main-content overflow-y-scroll w-100 p-3">
+            <div>
+                <div>
+                    <p>
+                        Dashboard page.
+                    </p>
+                </div>
+            </div>
+        </main>
+    </div>
 </body>
 
 </html>
